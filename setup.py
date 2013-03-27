1cf1a91f2ae4a4ad48ac9f2edac2bfa5622b4154
Cédric Messiant <cedricmessiant@ecreall.com>
2013-03-27 09:55:02 +0100

    Fixed

--- setup.py
+++ setup.py
@@ -41,8 +41,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'five.grok',
-        'plone.api',
         'setuptools',
         'collective.dms.basecontent',
         'collective.dms.mailcontent',
