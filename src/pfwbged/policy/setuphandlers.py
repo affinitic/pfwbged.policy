521dae8db5e43c65146e26400c7007ef8317b05e
Cédric Messiant <cedricmessiant@ecreall.com>
2013-03-28 11:54:50 +0100

    Remove Plone base folders from portal in postInstall

--- src/pfwbged/policy/setuphandlers.py
+++ src/pfwbged/policy/setuphandlers.py
@@ -9,3 +9,14 @@ def post_install(context):
     """Post install script"""
     if isNotCurrentProfile(context): return
     portal = context.getSite()
+
+    # Remove dummy content
+    if 'front-page' in portal:
+        portal.manage_delObjects(['front-page'])
+    if 'news' in portal:
+        portal.manage_delObjects(['news'])
+    if 'events' in portal:
+        portal.manage_delObjects(['events'])
+    if 'Members' in portal:
+        portal.Members.setExcludeFromNav(True)
+        portal.Members.reindexObject()
