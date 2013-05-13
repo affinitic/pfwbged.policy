f4c422fb6af52282a643ace9cfa15ef1a18fd1b0
Vincent Fretin <vincent.fretin@gmail.com>
2013-05-13 09:39:55 +0200

    Added viewlet 'go back to document'

--- src/pfwbged/policy/viewlets.py
+++ src/pfwbged/policy/viewlets.py
@@ -2,15 +2,20 @@ from Acquisition import aq_chain, aq_parent
 from zope.component import getUtility
 from zc.relation.interfaces import ICatalog
 from zope.intid.interfaces import IIntIds
+from zope.i18n import translate
 from five import grok
 
-from plone.app.layout.viewlets.interfaces import IBelowContentBody
+from plone.app.layout.viewlets.interfaces import (
+        IBelowContentBody, IAboveContentBody)
 
 from pfwbged.policy.interfaces import IIncomingMailAttributed
 from collective.task.content.task import ITask
 
 from collective.dms.mailcontent.dmsmail import IDmsIncomingMail
+from collective.dms.basecontent.dmsdocument import IDmsDocument
 from Products.CMFCore.utils import getToolByName
+
+from pfwbged.policy import _
 
 
 class CreateOutgoingMailViewlet(grok.Viewlet):
@@ -60,3 +65,20 @@ class CreateOutgoingMailViewlet(grok.Viewlet):
             self.recipients = '/'.join(incomingmail.sender.to_object.getPhysicalPath())
             self.in_reply_to = '/'.join(incomingmail.getPhysicalPath())
             self.treating_groups = self.context.responsible[0]
+
+
+class GoBackToDocumentViewlet(grok.Viewlet):
+    grok.context(ITask)
+    grok.viewletmanager(IAboveContentBody)
+
+    def render(self):
+        for obj in aq_chain(self.context):
+            obj = aq_parent(obj)
+            if IDmsDocument.providedBy(obj):
+                break
+
+        if not IDmsDocument.providedBy(obj):
+            return u""
+
+        title = translate(_(u"Go back to document"), context=self.request)
+        return u"""<a href="%s">%s</a>""" % (obj.absolute_url(), title)
