dfb53fdbb5ce32ec9968699cda639b826c941331
Cédric Messiant <cedricmessiant@ecreall.com>
2013-07-01 11:20:45 +0200

    Remove a bunch of unused stuff from ask_validation

--- src/pfwbged/policy/browser/ask_validation.py
+++ src/pfwbged/policy/browser/ask_validation.py
@@ -1,34 +1,17 @@
-from five import grok
-
-from z3c.form import button
-from z3c.form.field import Fields
-from z3c.form.interfaces import HIDDEN_MODE
-from zope import schema
 from zope.component import getUtility
 from zope.i18n import translate
 
-from Acquisition import aq_inner, aq_chain, aq_parent
+from Acquisition import aq_inner, aq_parent
 
 from plone import api
 from plone.dexterity.browser.add import DefaultAddForm
-from plone.dexterity.i18n import MessageFactory as DMF
 from plone.dexterity.interfaces import IDexterityFTI
 from plone.dexterity.utils import addContentToContainer, getAdditionalSchemata
-from plone.supermodel import model
-
-from Products.CMFPlone.utils import base_hasattr
-from Products.statusmessages.interfaces import IStatusMessage
 
 from collective.task.content.validation import IValidation
 
 from pfwbged.policy import _
 
-
-class IWorkflowAction(model.Schema):
-    """Simple schema that contains workflow action hidden field"""
-    workflow_action = schema.TextLine(title=_(u'Workflow action'),
-                                      required=False
-                                      )
 
 class AskValidation(DefaultAddForm):
     """Ask validation custom add form
