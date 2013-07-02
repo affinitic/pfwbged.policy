2b799ee2c3ab1c71e6b803e61720b0108a0840bb
Cédric Messiant <cedricmessiant@ecreall.com>
2013-07-02 16:30:00 +0200

    We don't need allow_discussion_default_value anymore

--- src/pfwbged/policy/customize.py
+++ src/pfwbged/policy/customize.py
@@ -1,15 +1,9 @@
-from plone.app.dexterity.behaviors.discussion import IAllowDiscussion
 from plone.directives.form.value import default_value
 from plone.directives.form.value import widget_label
 
 from collective.dms.basecontent.dmsdocument import IDmsDocument
 
 from . import _
-
-
-@default_value(field=IAllowDiscussion['allow_discussion'])
-def allow_discussion_default_value(data):
-    return True
 
 
 @widget_label(field=IDmsDocument['treating_groups'])
