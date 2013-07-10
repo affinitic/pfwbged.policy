005d9946ced3855c290421763e6f8e78bc53220d
Vincent Fretin <vincent.fretin@gmail.com>
2013-07-10 18:50:28 +0200

    be sure to not have duplicate

--- src/pfwbged/policy/subscribers/document.py
+++ src/pfwbged/policy/subscribers/document.py
@@ -25,7 +25,7 @@ def set_role_on_document(context, event):
     if not ITask.providedBy(context):
         document = context.getParentNode()
         if IDmsDocument.providedBy(document):
-            new_recipients = tuple(document.recipient_groups) + tuple(context.responsible)
+            new_recipients = tuple(frozenset(document.recipient_groups) + frozenset(context.responsible))
             cansee_dm = LocalRolesToPrincipalsDataManager(document, IDmsDocument['recipient_groups'])
             cansee_dm.set(new_recipients)
             document.reindexObjectSecurity()
