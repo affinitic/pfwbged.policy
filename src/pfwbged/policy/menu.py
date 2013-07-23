854c53bca0f23a57981e292944f98a872cf905ea
Frédéric Péters <fpeters@entrouvert.com>
2013-07-23 09:59:59 +0200

    add a common class to all actions on version (#3336)

--- src/pfwbged/policy/menu.py
+++ src/pfwbged/policy/menu.py
@@ -156,7 +156,7 @@ class CustomMenu(menu.WorkflowMenu):
                 version = context.Title()
                 title = dmsfile_wfactions_mapping[action['id']]
                 title = IGNORE_(title, mapping={'version': version})
-                cssClass += ' version-%s' % context.id
+                cssClass += ' version-action version-id-%s' % context.id
             else:
                 title = action['title']
 
