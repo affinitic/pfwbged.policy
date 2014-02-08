88203da760cf1be77632f2ca29374eefb04611b1
Frédéric Péters <fpeters@entrouvert.com>
2014-02-08 14:53:06 +0100

    make subpool folders redirect to their parent folder

--- src/pfwbged/policy/browser/documents.py
+++ src/pfwbged/policy/browser/documents.py
@@ -6,6 +6,7 @@ from plone import api
 from Products.CMFCore.utils import getToolByName
 
 from ..interfaces import IDocumentsFolder
+from ..interfaces import ISubpoolFolder
 
 class DocumentsView(grok.View):
     grok.context(IDocumentsFolder)
@@ -19,3 +20,17 @@ class DocumentsView(grok.View):
         typesTool = getToolByName(portal, 'portal_types')
         fti = typesTool.getTypeInfo('pfwbgedfolder')
         return ';'.join([x for x in fti.allowed_content_types if x not in ('pfwbgedfolder', 'pfwbgedlink',)])
+
+
+
+class SubpoolView(grok.View):
+    grok.context(ISubpoolFolder)
+    grok.name('edm_folder_listing')
+    grok.require('zope2.View')
+
+    def __call__(self):
+        new_url = self.context.absolute_url() + '/../'
+        self.context.REQUEST.response.redirect(new_url, lock=True)
+
+    def render(self):
+        return self()
