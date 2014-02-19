5bcd620f0566a7139b92e745e270b08445b89a83
Frédéric Péters <fpeters@entrouvert.com>
2014-02-19 09:33:18 +0100

    fix creation of folders for LDAP groups

--- src/pfwbged/policy/browser/utils.py
+++ src/pfwbged/policy/browser/utils.py
@@ -1,5 +1,6 @@
 from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory
+from zope.container.interfaces import INameChooser
 
 from Products.Five.browser import BrowserView
 
@@ -9,11 +10,15 @@ class ImportGroupFolders(BrowserView):
         portal = self.context
         factory = getUtility(IVocabularyFactory, 'plone.principalsource.Groups')
         vocabulary = factory(self.context)
+        chooser = INameChooser(self.context)
         for term in vocabulary:
             if term.value in ('Administrators', 'Reviewers',
                     'Site Administrators', 'AuthenticatedUsers'):
                 # ignore system groups
                 continue
+            if term.title.startswith('(Group) '):
+                term.value = chooser.chooseName(term.value, self.context)
+                term.title = term.title[len('(Group) '):] # remove '(Group) '
             if term.value in portal['dossiers']:
                 continue
             folder = portal['dossiers'].invokeFactory('pfwbgedfolder',
