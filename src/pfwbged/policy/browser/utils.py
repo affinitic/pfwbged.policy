8b87974c6237decde07c0f59a9adafe4bbf035f2
Frédéric Péters <fpeters@entrouvert.com>
2014-02-19 12:59:55 +0100

    make it possible to reset local roles on group folders

--- src/pfwbged/policy/browser/utils.py
+++ src/pfwbged/policy/browser/utils.py
@@ -3,12 +3,16 @@ from zope.schema.interfaces import IVocabularyFactory
 
 from Products.Five.browser import BrowserView
 
+from collective.z3cform.rolefield.field import LocalRolesToPrincipalsDataManager
+from pfwbged.folder import IFolder
+
 
 class ImportGroupFolders(BrowserView):
     def __call__(self):
         portal = self.context
         factory = getUtility(IVocabularyFactory, 'plone.principalsource.Groups')
         vocabulary = factory(self.context)
+        reset = self.request.form.get('reset') == 'true'
         for term in vocabulary:
             if term.value in ('Administrators', 'Reviewers',
                     'Site Administrators', 'AuthenticatedUsers'):
@@ -16,10 +20,25 @@ class ImportGroupFolders(BrowserView):
                 continue
             if term.title.startswith('(Group) '):
                 term.title = term.title[len('(Group) '):] # remove '(Group) '
+
             if term.value in portal['dossiers']:
-                continue
-            folder = portal['dossiers'].invokeFactory('pfwbgedfolder',
-                    term.value, title=term.title,
-                    treating_groups=[term.value],
-                    recipient_groups=[term.value])
+                if not reset:
+                    continue
+                folder = portal['dossiers'].get(term.value)
+            else:
+                folder = portal['dossiers'].invokeFactory('pfwbgedfolder',
+                    term.value, title=term.title)
+
+            canwrite_dm = LocalRolesToPrincipalsDataManager(folder,
+                        IFolder['treating_groups'])
+            canread_dm = LocalRolesToPrincipalsDataManager(folder,
+                        IFolder['recipient_groups'])
+            if reset:
+                canwrite_dm.set([])
+            canwrite_dm.set([term.value])
+            if reset:
+                canread_dm.set([])
+            canread_dm.set([term.value])
+            folder.reindexObjectSecurity()
+
         return 'OK'
