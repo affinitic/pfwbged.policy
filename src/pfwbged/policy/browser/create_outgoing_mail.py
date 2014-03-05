b1b66bef4938c6f1a54f911b7f54202fc1038596
Frédéric Péters <fpeters@entrouvert.com>
2014-03-05 16:20:05 +0100

    create outgoing mail from main documents folder, not current folder

--- src/pfwbged/policy/browser/create_outgoing_mail.py
+++ src/pfwbged/policy/browser/create_outgoing_mail.py
@@ -48,6 +48,6 @@ form.widgets.recipients:list=%(recipients)s&
 form.widgets.in_reply_to:list=%(in_reply_to)s&
 form.widgets.IRelatedTask.related_task=%(related_task)s&
 form.widgets.treating_groups=%(treating_groups)s""" % values
-        folder_url = incomingmail.getParentNode().absolute_url()
+        folder_url = api.portal.get()['documents'].absolute_url()
         url = folder_url + "/++add++dmsoutgoingmail?" + values_url.encode('utf-8')
         self.request.response.redirect(url)
